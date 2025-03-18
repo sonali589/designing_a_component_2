28b94e2be448d07e597c5ac3c8523d365810b572
PV SONALI <pv.sonali.official@gmail.com>
2025-03-18 10:35:58 +0530

    added productcard page

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,11 @@
-// write button card here
+import React from "react";
+
+const ViewProductButton = () => {
+  return (
+    <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+      View Product
+    </button>
+  );
+};
+
+export default ViewProductButton;
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,23 @@
-// write product card here
+import React from "react";
+import ViewProductButton from "../components/button"; // Import button component
+
+const Productcard = () => {
+  
+  const productImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe-Dc9KLtAheIeVKV0gcGL_BSl81q3r2O0sg&s";
+  const productName = "MacBook Air M3";
+  const productPrice = "$999";
+
+  return (
+    <div className="border p-4 rounded-lg shadow-md max-w-sm">
+      <img src={productImage} alt={productName} className="w-full h-48 object-cover rounded-md" />
+      <h2 className="text-lg font-semibold mt-2">{productName}</h2>
+      <p className="text-gray-600">{productPrice}</p>
+      <div className="mt-4">
+        <ViewProductButton />
+      </div>
+    </div>
+  );
+};
+
+export default Productcard;
+
